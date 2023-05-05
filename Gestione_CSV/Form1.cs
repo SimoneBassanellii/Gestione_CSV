1054912f80d0735cd1fc6aa9179f62ad89ad2930
bassanelli.20180 <bassanelli.simone.studente@itispaleocapa.it>
2023-05-05 13:14:53 +0200

    Funzione aggiunta

--- Gestione_CSV/Form1.cs
+++ Gestione_CSV/Form1.cs
@@ -25,13 +25,21 @@ namespace Gestione_CSV
 
         // Carica il file CSV
         public string FileName = "@bassanelli.csv";
-        StreamWriter writer = new StreamWriter(FileName, append: false);
 
         Random rnd = new Random();
 
         private void Aggiunta_mio_valore_Click(object sender, EventArgs e)
         {
+            StreamReader sr = new StreamReader(FileName);
+            string vs = sr.ReadToEnd();
+            string[] no = vs.Split(';');
 
+            
+            StreamWriter writer = new StreamWriter(FileName, append: false);
+            for (int i = 0; i < no.Length; i++)
+            {
+                writer.WriteLine(rnd.Next(10,20)+";0");
+            }
         }
     }
 }
